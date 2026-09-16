afd63987f3724b055b2b64c7ea24541ad2731489
Darragh <126518729@umail.ucc.ie>
2026-09-16 22:15:00 +0100

    First commit : hello world

---
+++ hello.py
@@ -0,0 +1,8 @@
+#my first python program
+name = input("Enter your name: ")
+print("Hello, " + name + "! Welcome to Python programming.")
+
+#calculate the area of a circle
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * radius ** 2
+print("The area of the circle is: ", area)
